dcbadd851baf8bc2762b7fb98aa06ee8a9d951e7
Pliniodev <plinioprojetos@hotmail.com>
2021-06-20 15:08:11 -0300

    Creating some constants to use for step numbers

---
+++ app/src/main/java/com/pliniodev/gametest/constants/Constants.kt
@@ -0,0 +1,9 @@
+package com.pliniodev.gametest.constants
+
+object Constants {
+    const val STEP5 = 5
+    const val STEP4 = 4
+    const val STEP3 = 3
+    const val STEP2 = 2
+    const val STEP1 = 1
+}
