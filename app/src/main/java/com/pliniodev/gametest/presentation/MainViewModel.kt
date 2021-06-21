196c4632628d06f49e83a032ce319bbc287701d0
Pliniodev <plinioprojetos@hotmail.com>
2021-06-20 21:52:31 -0300

    Adding shared pref

--- app/src/main/java/com/pliniodev/gametest/presentation/MainViewModel.kt
+++ app/src/main/java/com/pliniodev/gametest/presentation/MainViewModel.kt
@@ -1,9 +1,9 @@
 package com.pliniodev.gametest.presentation
 
+import android.content.SharedPreferences
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.pliniodev.gametest.constants.Constants
 import com.pliniodev.gametest.data.local.model.StepModel
 import com.pliniodev.gametest.data.local.repository.StepRepository
 import com.pliniodev.gametest.domain.usecase.GetPhraseUseCase
@@ -12,44 +12,52 @@ import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val useCase: GetPhraseUseCase,
-    private val database: StepRepository
-): ViewModel() {
+    private val database: StepRepository,
+    private val shared: SharedPreferences,
+    private var sharedEditor: SharedPreferences.Editor
+) : ViewModel() {
+
     val lastSelectedStepLiveData = MutableLiveData<Int>()
     val phraseLiveData = MutableLiveData<String>()
     val updatedStep = MutableLiveData<Int>()
     val errorLiveData = MutableLiveData<String>()
     private val phraseMutableList = mutableListOf<String>()
 
-    /**
-     * Requisita o ultimo step selecionado no bd, o padrão é 0
-     */
-    fun requestLastStepSelected() {//requisita o último step selecionado bd
-        lastSelectedStepLiveData.value = Constants.STEP1//caso retorne 1
+    fun requestLastStepSelected() {
+        val lastStepSelected = shared.getInt("lastStepSelected", 1)
+        lastSelectedStepLiveData.postValue(lastStepSelected)
     }
 
-    fun updateSelectedStep(stepSelected: Int) { //escreve no banco de dados
+    fun updateSelectedStep(stepSelected: Int) {
+        sharedEditor = shared.edit()
+        sharedEditor.putInt("lastStepSelected", stepSelected)
+        sharedEditor.apply()
         updatedStep.value = stepSelected
     }
 
-    //request ao banco de dados
-    fun getPhrase(step: Int) {
-
+    fun getPhrase(stepNumber: Int) {
+        phraseLiveData.postValue(database.getPhrase(stepNumber).phrase)
     }
 
     fun updateBD() {
-        //todo passou um dia? faça
-        viewModelScope.launch(Dispatchers.IO) {
-            runCatching {
-                useCase()
-            }.onSuccess { presentationList ->
-                for (phrase in presentationList){
-                    phraseMutableList.add(phrase.text)
+        if (isOlderOneDay()){
+            viewModelScope.launch(Dispatchers.IO) {
+                runCatching {
+                    useCase()
+                }.onSuccess { presentationList ->
+                    for (phrase in presentationList) {
+                        phraseMutableList.add(phrase.text)
+                    }
+                    updatePhrasesOnBd(phraseMutableList)
+                }.onFailure {
+                    errorLiveData.postValue(it.message)
                 }
-                updatePhrasesOnBd(phraseMutableList)
-            }.onFailure {
-                errorLiveData.postValue(it.message)
             }
         }
+    }
+
+    private fun isOlderOneDay(): Boolean {
+        TODO("Not yet implemented")
     }
 
     private fun updatePhrasesOnBd(phraseMutableList: MutableList<String>) {
